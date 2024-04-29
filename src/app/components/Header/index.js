750305e93a2c868f9d7cbe361873552214e22a5e
rehman25 <a.rehman.mblk@gmail.com>
2024-04-29 20:00:56 +0400

    rehman

--- src/app/components/Header/index.js
+++ src/app/components/Header/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './header.module.css'
 
 const Index = () => {
     return (
-        <div className='container'>
-            
+        <div className={`container ${style.container}`}>
+            <h1>adf</h1>
         </div>
     );
 }
